8d48cbfa182c18c73b20a190a0a953f2e96c5d5f
Bobby Jap <RWJap85@gmail.com>
2016-11-14 21:23:28 -0800

    Initial project and gitignore commit

---
+++ SwiftCrypt/SwiftCrypt/SwiftCrypt.h
@@ -0,0 +1,13 @@
+//
+//  SwiftCrypt.h
+//  SwiftCrypt
+//
+//  Created by Bobby Jap on 11/14/16.
+//  Copyright © 2016 Bobby Jap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwiftCrypt : NSObject
+
+@end
---
+++ SwiftCrypt/SwiftCrypt/SwiftCrypt.m
@@ -0,0 +1,13 @@
+//
+//  SwiftCrypt.m
+//  SwiftCrypt
+//
+//  Created by Bobby Jap on 11/14/16.
+//  Copyright © 2016 Bobby Jap. All rights reserved.
+//
+
+#import "SwiftCrypt.h"
+
+@implementation SwiftCrypt
+
+@end
